6dc070718b830ba366fab3007cc095047b113aa0
15903031938 <l7492541>
2020-05-20 17:07:57 +0800

    "05-20"

--- src/main/java/com/lzq/springbootmybatis01/util/impl/TestImpl.java
+++ src/main/java/com/lzq/springbootmybatis01/util/impl/TestImpl.java
@@ -2,6 +2,7 @@ package com.lzq.springbootmybatis01.util.impl;
 
 import com.lzq.springbootmybatis01.util.Strategry;
 import com.lzq.springbootmybatis01.util.Type;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
  * @create: 2020-05-10 17:48
  **/
 @Type(2)
+@Service
 public class TestImpl implements Strategry {
     @Override
     public BigDecimal calRecharge(Integer chanId, Integer goodsId) {
